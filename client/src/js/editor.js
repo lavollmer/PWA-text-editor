ebd534b3a166b3e34425cb5cf0fc517ab9a49b77
lavollmer <141365306+lavollmer@users.noreply.github.com>
2023-11-28 16:27:13 -0600

    added hw helper code from gary

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -1,4 +1,7 @@
-// Import methods to save and get data from the indexedDB database in './database.js'
+/*
+  This is the actual application code. No work for you here. (Sorry.)
+*/
+
 import { getDb, putDb } from './database';
 import { header } from './header';
 
@@ -22,8 +25,7 @@ export default class {
       tabSize: 2,
     });
 
-    // When the editor is ready, set the value to whatever is stored in indexeddb.
-    // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
+    // When the editor is ready, set the value to whatever is store in indexeddb
     getDb().then((data) => {
       console.info('Loaded data from IndexedDB, injecting into editor');
       this.editor.setValue(data || localData || header);
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,29 @@
 const butInstall = document.getElementById('buttonInstall');
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+/* Here we need to provide the logic for when and how to install the PWA.  */
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+//  Keep this code as-is
+window.addEventListener('beforeinstallprompt', (event) => {
+  window.deferredPrompt = event;
+  butInstall.classList.toggle('hidden', false);
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+// This listener fires when the user clicks the "install" button.
+butInstall.addEventListener('click', async () => {
+  const promptEvent = window.deferredPrompt;
+
+  if (!promptEvent) {
+    return;
+  }
+
+  promptEvent.prompt();
+
+  // TODO: Notice line 7 above.  window.deferredPrompt should now be set to null.
+
+  // TODO: Notice line 8 above. We want the same line here, but the hidden value should be set to true.
+  // Insert line here
+});
+
+window.addEventListener('appinstalled', (event) => {
+  window.deferredPrompt = null;
+});
